56baeadbec0003b2988c1e287bec242c143a3267
changsuwei <F74036255@mail.ncku.edu.tw>
2017-02-19 14:38:18 +0800

    test

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def webhook():
                     message_text = messaging_event["message"]["text"]  # the message's text
                     if(message_text=="Hello"):
                         send_message(sender_id, "87")
-                    send_message(sender_id, "got it, thanks!")
+                    send_message(sender_id, "got it")
 
                 if messaging_event.get("delivery"):  # delivery confirmation
                     pass
