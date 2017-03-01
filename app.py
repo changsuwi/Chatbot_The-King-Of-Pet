b3944f61529c177cc544f8b0502e81fb74b05d43
changsuwi <F74036255@mail.ncku.edu.tw>
2017-03-01 14:29:19 +0800

    test

--- app.py
+++ app.py
@@ -52,9 +52,9 @@ def webhook():
                         elif(number==3):
                             send_message(sender_id,"87人類")
                     else:
-                        res=requests.get("http://asms.wsn.com.tw/tn/ieland/el_LoseList.aspx")
+                        res=requests.get("http://asms.wsn.com.tw/tn/ieland/el_LoseList.aspx",timeout=3000000)
                         print("success")
-                        soup = BeautifulSoup(res.text,"lxml",timeout=100) 
+                        soup = BeautifulSoup(res.text,"lxml") 
                         for item in soup.select(".divAnimalList"):
                             print item.select("input")[2].get('src')
                             print item.select("li")[0].text,item.select("li")[1].text,item.select("li")[2].text,item.select("li")[3].text,item.select("li")[5].text
