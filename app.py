0018a42f9de00b2545858f414d340d622c334047
changsuwei <F74036255@mail.ncku.edu.tw>
2017-03-12 22:39:44 +0800

    程式結構化

--- app.py
+++ app.py
@@ -40,49 +40,25 @@ def webhook():
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     message_text = messaging_event["message"]["text"]  # the message's text
-                    print(sender_id)
+                    print(sender_id) #test
                     if(message_text=="Hello" or message_text=="Hi" or message_text==u"嗨" or message_text==u"妳好" or message_text==u"你好" or message_text=="hello" or message_text=="hi"):
-                        send_message(sender_id, "汪汪")
-                        send_mainbutton(sender_id)
+                        json_message(sender_id, "汪汪")
+                        json_mainbutton(sender_id)
                     elif(message_text==u"幹" or message_text==u"靠杯" or message_text==u"靠北" or message_text==u"87"):
                         number=random.randint(0, 3)
                         if(number==0):
-                            send_message(sender_id, "不給骨頭就咬你")
+                            json_message(sender_id, "不給骨頭就咬你")
                         elif(number==1):
-                            send_message(sender_id, "咬你喔")
+                            json_message(sender_id, "咬你喔")
                         elif(number==2):
-                            send_message(sender_id,"嗚嗚")
+                            json_message(sender_id,"嗚嗚")
                         elif(number==3):
-                            send_message(sender_id,"87人類")
+                            json_message(sender_id,"87人類")
                     elif(message_text==u"領養資訊搜尋"):
-                        template = {
-                                "recipient": {
-                                        "id": sender_id
-                                        },
-                                        "message":{
-                                                "attachment":{
-                                                        "type":"template",
-                                                        "payload":{
-                                                                "template_type":"generic",
-                                                                "elements":[
-                                                                        ]
-                                                                }
-                                                        
-                                            }
-                                }
-                        } 
-                        res=requests.get("http://animal-adoption.coa.gov.tw/index.php/animal?s_area=16&s_kind=%E7%8B%97&s_bodytype=SMALL&num=8&s_color=CHILD&s_color=ALL&s_sex=F")
-                        soup = BeautifulSoup(res.text,"lxml") 
-                        for item in soup.select(".an"):
-                             location=item.select(".area")[0].text.encode("utf-8")
-                             gender=item.select(".gender")[0].text.encode("utf-8")
-                             shelter=item.select(".shelters")[0].text.encode("utf-8")
-                             image_url=item.select("img")[0].get('data-original')
-                             item_url=item.select("a")[0].get('href')
-                             template=add_template(template,location,gender,shelter,item_url,image_url)
-                        send_template(template,sender_id,location,gender,shelter,item_url,image_url)
+                        crawler(sender_id);
+                        
                     else:
-                        send_message(sender_id,"好的")
+                        json_message(sender_id,"好的")
                         
                 if messaging_event.get("delivery"):  # delivery confirmation
                     pass
@@ -94,7 +70,40 @@ def webhook():
                     pass
 
     return "ok", 200
+
+def crawler(sender_id):
+    # the animal adoption imformation is crawlered by  http://animal-adoption.coa.gov.tw
+    # this function construct a main template and start to crawler
+    template = {
+                    "recipient": {
+                    "id": sender_id
+                    },
+                    "message":{
+                        "attachment":{
+                                "type":"template",
+                                "payload":{
+                                            "template_type":"generic",
+                                            "elements":[
+                                                    ]
+                                            }
+                                    }
+                                }
+                } 
+    # start to crawler
+    res=requests.get("http://animal-adoption.coa.gov.tw/index.php/animal?s_area=16&s_kind=%E7%8B%97&s_bodytype=SMALL&num=8&s_color=CHILD&s_color=ALL&s_sex=F")
+    soup = BeautifulSoup(res.text,"lxml") 
+    for item in soup.select(".an"):
+        location=item.select(".area")[0].text.encode("utf-8")
+        gender=item.select(".gender")[0].text.encode("utf-8")
+        shelter=item.select(".shelters")[0].text.encode("utf-8")
+        image_url=item.select("img")[0].get('data-original')
+        item_url=item.select("a")[0].get('href')
+        template=add_template(template,location,gender,shelter,item_url,image_url) #find new imformation,so add this in the template
+    json_template(template,sender_id)
+    
+    
 def add_template(template,location,gender,shelter,item_url,image_url):
+    #add new information in to the template
     bobble={
         "title":"寵物",
         "image_url":image_url,
@@ -111,30 +120,13 @@ def add_template(template,location,gender,shelter,item_url,image_url):
     template["message"]["attachment"]["payload"]["elements"].append(bobble)
     return template
 
-def send_template(template,recipient_id,location,gender,shelter,item_url,image_url):
-    log("sending  to {recipient}".format(recipient=recipient_id))
-
-    params = {
-        "access_token": os.environ["PAGE_ACCESS_TOKEN"]
-    }
-    headers = {
-        "Content-Type": "application/json"
-    }
+def json_template(template,recipient_id): #construct template json
+    log("sending  template to {recipient}".format(recipient=recipient_id))
     data = json.dumps(template)
-    r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
-    if r.status_code != 200:
-        log(r.status_code)
-        log(r.text)
-def send_mainbutton(recipient_id):
-    log("sending message to {recipient}".format(recipient=recipient_id))
-
-    params = {
-        "access_token": os.environ["PAGE_ACCESS_TOKEN"]
-    }
-    headers = {
-        "Content-Type": "application/json"
-    }
-    
+    sendtofb(data)
+        
+def json_mainbutton(recipient_id): #construct mainbutton json
+    log("sending mainbutton to {recipient}".format(recipient=recipient_id))
     data=json.dumps(
             {"recipient":{
     "id": recipient_id
@@ -161,20 +153,11 @@ def send_mainbutton(recipient_id):
   }
     }
     )
-    r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
-    if r.status_code != 200:
-        log(r.status_code)
-        log(r.text)       
-def send_message(recipient_id, message_text):
+    sendtofb(data)
+        
+def json_message(recipient_id, message_text): #construct message json
 
     log("sending message to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
-
-    params = {
-        "access_token": os.environ["PAGE_ACCESS_TOKEN"]
-    }
-    headers = {
-        "Content-Type": "application/json"
-    }
     data = json.dumps({
         "recipient": {
             "id": recipient_id
@@ -183,12 +166,20 @@ def send_message(recipient_id, message_text):
             "text": message_text
         }
     })
+    sendtofb(data)
+
+def sendtofb(data): #send json to facebook
+    params = {
+        "access_token": os.environ["PAGE_ACCESS_TOKEN"]
+    }
+    headers = {
+        "Content-Type": "application/json"
+    }
     r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
     if r.status_code != 200:
         log(r.status_code)
         log(r.text)
-
-
+        
 def log(message):  # simple wrapper for logging to stdout on heroku
     print str(message)
     sys.stdout.flush()
