e6ca12a166991dcda891c9c9cbe04cb560a24b8e
changsuwei <F74036255@mail.ncku.edu.tw>
2017-03-22 17:15:13 +0800

    debug

--- app.py
+++ app.py
@@ -54,7 +54,8 @@ def webhook():
                             json_message(sender_id,"嗚嗚")
                         elif(number==3):
                             json_message(sender_id,"87人類")
-                    
+                    elif(sender_id=="1241168685962318"):
+                        json_message(sender_id,"嗚嗚")
                     elif(message_text==u"領養資訊搜尋"):
                         json_searchlocation(sender_id)
                     elif(message_text==u"民間送養資訊搜尋"):
