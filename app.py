52260681fdd4c9c3f69bb126c2a43d0e1bd56d2c
changsuwei <F74036255@mail.ncku.edu.tw>
2017-05-05 05:24:39 +0800

    test

--- app.py
+++ app.py
@@ -39,8 +39,6 @@ def webhook():
     # endpoint for processing incoming messaging events
     data = request.get_json()
     log(data)  # you may not want to log every incoming message in production, but it's good for testing
-    get_start()
-    set_manu()
     if data["object"] == "page":
 
         for entry in data["entry"]:
