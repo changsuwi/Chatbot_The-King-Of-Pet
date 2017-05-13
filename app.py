8d55431011741f27786b433596424136b5768761
changsuwei <F74036255@mail.ncku.edu.tw>
2017-05-14 01:37:49 +0800

    排版

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ app = Flask(__name__)
 def verify():
     # when the endpoint is registered as a webhook, it must echo back
     # the 'hub.challenge' value it receives in the query arguments
-    if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
+    if (request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge")):
         if not request.args.get("hub.verify_token") == os.environ["VERIFY_TOKEN"]:
             return "Verification token mismatch", 403
         return request.args["hub.challenge"], 200
@@ -40,9 +40,11 @@ def webhook():
             for messaging_event in entry["messaging"]:
 
                 if messaging_event.get("message"):  # someone sent us a message
-                    sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
+                    # the facebook ID of the person sending you the message
+                    sender_id = messaging_event["sender"]["id"]
                     if("text" in messaging_event["message"]):
-                        message_text = messaging_event["message"]["text"]  # the message's text
+                        message_text = messaging_event["message"][
+                            "text"]  # the message's text
                         print(sender_id)  # test
                         if(message_text == "Hello" or message_text == "Hi" or message_text == u"嗨" or message_text == u"妳好" or message_text == u"你好" or message_text == "hello" or message_text == "hi" or message_text == u"哈囉"):
                             upload_flag(0, sender_id)
@@ -60,7 +62,8 @@ def webhook():
                                 json_message(sender_id, "87人類")
                         elif(message_text == u"聊天"):
                             upload_flag(1, sender_id)
-                            json_message(sender_id, "我懂很多寵物知識喔\n你可以問我有關寵物領養 寵物健康 寵物食品的各種問題~")
+                            json_message(
+                                sender_id, "我懂很多寵物知識喔\n你可以問我有關寵物領養 寵物健康 寵物食品的各種問題~")
                         elif(message_text == u"交換明信片"):
                             upload_flag(2, sender_id)
                             json_message(sender_id, "請先傳送一張寵物的可愛照吧~")
@@ -79,33 +82,40 @@ def webhook():
                             elif(get_flag(sender_id) == 2):
                                 upload_db_intro(message_text, sender_id)
                                 json_message(sender_id, "已完成，請耐心等待神秘的明信片")
-                            elif(get_flag(sender_id) == 4 or get_flag(sender_id) == 5):
-                                #  use payload to save the data which user send
-                                #  領養資訊搜尋：location---> city ---> kind ---> body ---> start crawler
-                                #  民間送養資訊：kind---> location---> city---> start crawler
+                            elif(get_flag(sender_id) == 4 or
+                                 get_flag(sender_id) == 5):
+                                '''
+                            use payload to save the data which user send
+                            領養資訊搜尋：location->city->kind->body->start crawler
+                            民間送養資訊：kind->location->city-> start crawler
+                                '''
                                 if(message_text == u"北部地區" or message_text == u"中部地區" or message_text == u"南部地區" or message_text == u"東部地區"):
-                                    payload = messaging_event["message"]["quick_reply"]["payload"]
+                                    payload = messaging_event["message"][
+                                        "quick_reply"]["payload"]
                                     print "payload={a}".format(a=payload)
                                     if(payload == "1" or payload == "2" or payload == "3" or payload == "4"):
                                         json_city(sender_id, payload)
                                     else:
                                         json_city2(sender_id, payload)
                                 elif(u"縣" in message_text or u"市" in message_text or message_text == u"北北基宜全部" or message_text == u"桃竹苗全部" or message_text == u"中彰投全部" or message_text == u"雲嘉南全部" or message_text == u"高屏全部" or message_text == u"花東全部"):
-                                    payload = messaging_event["message"]["quick_reply"]["payload"]
+                                    payload = messaging_event["message"][
+                                        "quick_reply"]["payload"]
                                     if(len(payload) <= 5):
                                         json_searchdogcat(sender_id, payload)
                                     else:
                                         typingon_json(sender_id)
                                         crawler2(sender_id, payload)
                                 elif(message_text == u"全部種類" or message_text == u"狗" or message_text == u"貓"):
-                                    payload = messaging_event["message"]["quick_reply"]["payload"]
+                                    payload = messaging_event["message"][
+                                        "quick_reply"]["payload"]
                                     if(payload == "dog " or payload == "cat "):
                                         json_location2(sender_id, payload)
                                     else:
                                         json_searchbodytype(sender_id, payload)
                                 elif(message_text == u"全部體型" or message_text == u"迷你型" or message_text == u"小型" or message_text == u"中型" or message_text == u"大型"):
                                     typingon_json(sender_id)
-                                    searchlist = messaging_event["message"]["quick_reply"]["payload"]
+                                    searchlist = messaging_event[
+                                        "message"]["quick_reply"]["payload"]
                                     crawler(sender_id, searchlist)
                     elif("attachments" in messaging_event["message"] and get_flag(sender_id) == 2):
                         for attachment in messaging_event["message"]["attachments"]:
@@ -122,7 +132,8 @@ def webhook():
                 if messaging_event.get("optin"):  # optin confirmation
                     pass
 
-                if messaging_event.get("postback"):  # user clicked/tapped "postback" button in earlier message
+                # user clicked/tapped "postback" button in earlier message
+                if messaging_event.get("postback"):
                     pass
 
     return "ok", 200
--- db.py
+++ db.py
@@ -4,44 +4,39 @@
 import pymongo
 
 
-### Standard URI format: mongodb://[dbuser:dbpassword@]host:port/dbname
+#  Standard URI format: mongodb://[dbuser:dbpassword@]host:port/dbname
 
-uri = 'mongodb://vic010744:vic32823@ds135700.mlab.com:35700/heroku_4w25h5pt' 
+uri = 'mongodb://vic010744:vic32823@ds135700.mlab.com:35700/heroku_4w25h5pt'
 
 ###############################################################################
 # main
 ###############################################################################
 
-def upload_flag(flag,sender_id):
-    client = pymongo.MongoClient(uri)
 
+def upload_flag(flag, sender_id):
+    client = pymongo.MongoClient(uri)
     db = client.get_default_database()
-    
     Category = db['flag']
-    
     query = {'ID': sender_id}
-    if(Category.count(query)==0):
-        Category.insert_one({'ID': sender_id,'flag': flag})
+    if(Category.count(query) == 0):
+        Category.insert_one({'ID': sender_id, 'flag': flag})
     else:
         query = {'ID': sender_id}
-        Category.update(query,{'$set': {'flag': flag}})
-    
+        Category.update(query, {'$set': {'flag': flag}})
+
+
 def get_flag(sender_id):
     client = pymongo.MongoClient(uri)
-
     db = client.get_default_database()
-    
     Category = db['flag']
     dat = Category.find_one({'ID': sender_id})
     return dat['flag']
 
-def upload_db_photo_url(url,sender_id):
 
+def upload_db_photo_url(url, sender_id):
     client = pymongo.MongoClient(uri)
-
     db = client.get_default_database()
-    
-    # First we'll add a few songs. Nothing is required to create the songs 
+    # First we'll add a few songs. Nothing is required to create the songs
     # collection; it is created automatically when we insert.
 
     Postcard = db['postcard']
@@ -49,45 +44,24 @@ def upload_db_photo_url(url,sender_id):
     # Note that the insert method can take either an array or a single dict.
     query = {'ID': sender_id}
     if(Postcard.count(query) == 0):
-        
-        SEED_DATA={
-                'url': url,
-                'ID': sender_id
-                }
+        SEED_DATA = {
+            'url': url,
+            'ID': sender_id
+        }
         print Postcard.insert_one(SEED_DATA)
     else:
-        Postcard.update(query,{'$set': {'url': url}})
-def upload_db_intro(text,sender_id):
+        Postcard.update(query, {'$set': {'url': url}})
+
+
+def upload_db_intro(text, sender_id):
     client = pymongo.MongoClient(uri)
 
     db = client.get_default_database()
-    
-    # First we'll add a few songs. Nothing is required to create the songs 
+    # First we'll add a few songs. Nothing is required to create the songs
     # collection; it is created automatically when we insert.
 
     Postcard = db['postcard']
     query = {'ID': sender_id}
-    Postcard.update(query,{'$set': {'intro': text,'match': '0','match_id': "None"}})
-    # Then we need to give Boyz II Men credit for their contribution to
-    # the hit "One Sweet Day".
-
-    #query = {'song': 'One Sweet Day'}
-
-    #songs.update(query, {'$set': {'artist': 'Mariah Carey ft. Boyz II Men'}})
-
-    # Finally we run a query which returns all the hits that spent 10 or
-    # more weeks at number 1.
-
-    #cursor = songs.find({'weeksAtOne': {'$gte': 10}}).sort('decade', 1)
-
-    #for doc in cursor:
-     #   print ('In the %s, %s by %s topped the charts for %d straight weeks.' %
-      #         (doc['decade'], doc['song'], doc['artist'], doc['weeksAtOne']))
-    
-    ### Since this is an example, we'll clean up after ourselves.
-
-    ###db.drop_collection('songs')
-
-    ### Only close the connection when your app is terminating
-
+    Postcard.update(
+        query, {'$set': {'intro': text, 'match': '0', 'match_id': "None"}})
     client.close()
