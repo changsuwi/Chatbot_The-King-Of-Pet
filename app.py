491caf88f3daf57bf84f43e911dbbd38cfe29e63
changsuwei <F74036255@mail.ncku.edu.tw>
2017-05-20 17:23:53 +0800

    test

--- app.py
+++ app.py
@@ -134,8 +134,22 @@ def webhook():
 
                 # user clicked/tapped "postback" button in earlier message
                 if messaging_event.get("postback"):
-                    pass
-
+                    if messaging_event["postback"]["payload"] == 'main_button1':
+                        upload_flag(1, sender_id)
+                        json_message(
+                            sender_id, "我懂很多寵物知識喔\n你可以問我有關寵物領養 寵物健康 寵物食品的各種問題~")
+                    elif messaging_event["postback"]["payload"] == 'main_button2':
+                        upload_flag(2, sender_id)
+                        json_message(sender_id, "請先傳送一張寵物的可愛照吧~")
+                    elif messaging_event["postback"]["payload"] == 'main_button3':
+                        upload_flag(3, sender_id)
+                        json_video(sender_id)
+                     elif messaging_event["postback"]["payload"] == 'main_button4':
+                        upload_flag(4, sender_id)
+                        json_location(sender_id)
+                     elif messaging_event["postback"]["payload"] == 'main_button5':
+                        upload_flag(5, sender_id)
+                        json_choosedogcat2(sender_id)
     return "ok", 200
 
 
--- json_fb.py
+++ json_fb.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Mar 28 16:50:20 2017
-json_fb 此模組主要存放通用的json格式與打包 例如使用者輸入json ，template打包 ， main button 的json ，message的打包
+json_fb 此模組主要存放通用的json格式與打包
+例如使用者輸入json ，template打包 ， main button 的json ，message的打包
 @author: vicharm
 """
 from sendtofb_log import sendtofb, log
@@ -11,7 +12,8 @@ import json
 def typingon_json(recipient_id):
     #  construct typing on json
     log("sending  typingon to {recipient}".format(recipient=recipient_id))
-    data = json.dumps({"recipient": {"id": recipient_id}, "sender_action": "typing_on"})
+    data = json.dumps({"recipient": {"id": recipient_id},
+                       "sender_action": "typing_on"})
     sendtofb(data)
 
 
@@ -85,7 +87,8 @@ def json_video(recipient_id):
 
 def json_message(recipient_id, message_text):  # construct message json
 
-    log("sending message to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
+    log("sending message to {recipient}: {text}".format(
+        recipient=recipient_id, text=message_text))
     data = json.dumps({
         "recipient": {
             "id": recipient_id
