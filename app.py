0218a136f1e259068d66725492758834c27ed4fe
changsuwi <F74036255@mail.ncku.edu.tw>
2017-06-11 16:28:51 +0800

    測試

--- app.py
+++ app.py
@@ -46,78 +46,47 @@ def webhook():
                         message_text = messaging_event["message"][
                             "text"]  # the message's text
                         print(sender_id)  # test
-                        if(message_text == "Hello" or message_text == "Hi" or message_text == u"嗨" or message_text == u"妳好" or message_text == u"你好" or message_text == "hello" or message_text == "hi" or message_text == u"哈囉"):
-                            upload_flag(0, sender_id)
-                            json_message(sender_id, "汪汪")
-                            json_mainbutton(sender_id)
-                        elif(message_text == u"幹" or message_text == u"靠杯" or message_text == u"靠北" or message_text == u"87"):
-                            number = random.randint(0, 3)
-                            if(number == 0):
-                                json_message(sender_id, "不給骨頭就咬你")
-                            elif(number == 1):
-                                json_message(sender_id, "咬你喔")
-                            elif(number == 2):
-                                json_message(sender_id, "嗚嗚")
-                            elif(number == 3):
-                                json_message(sender_id, "87人類")
-                        elif(message_text == u"聊天"):
-                            upload_flag(1, sender_id)
-                            json_message(
-                                sender_id, "我懂很多寵物知識喔\n你可以問我有關寵物領養 寵物健康 寵物食品的各種問題~")
-                        elif(message_text == u"交換明信片"):
-                            upload_flag(2, sender_id)
-                            json_message(sender_id, "請先傳送一張寵物的可愛照吧~")
-                        elif(message_text == u"可愛寵物影片推播"):
-                            upload_flag(3, sender_id)
-                            videos = get_video()
-                            deal_video(sender_id, videos)
-                        elif(message_text == u"領養資訊搜尋"):
-                            upload_flag(4, sender_id)
-                            json_location(sender_id)
-                        elif(message_text == u"民間送養資訊搜尋"):
-                            upload_flag(5, sender_id)
-                            json_choosedogcat2(sender_id)
-                        else:
-                            if(get_flag(sender_id) == 1):
-                                chat(sender_id, message_text)
-                            elif(get_flag(sender_id) == 2):
-                                upload_db_intro(message_text, sender_id)
-                                json_message(sender_id, "已完成，請耐心等待神秘的明信片")
-                            elif(get_flag(sender_id) == 4 or
-                                 get_flag(sender_id) == 5):
-                                '''
-                            use payload to save the data which user send
-                            領養資訊搜尋：location->city->kind->body->start crawler
-                            民間送養資訊：kind->location->city-> start crawler
-                                '''
-                                if(message_text == u"北部地區" or message_text == u"中部地區" or message_text == u"南部地區" or message_text == u"東部地區"):
-                                    payload = messaging_event["message"][
-                                        "quick_reply"]["payload"]
-                                    print "payload={a}".format(a=payload)
-                                    if(payload == "1" or payload == "2" or payload == "3" or payload == "4"):
-                                        json_city(sender_id, payload)
-                                    else:
-                                        json_city2(sender_id, payload)
-                                elif(u"縣" in message_text or u"市" in message_text or message_text == u"北北基宜全部" or message_text == u"桃竹苗全部" or message_text == u"中彰投全部" or message_text == u"雲嘉南全部" or message_text == u"高屏全部" or message_text == u"花東全部"):
-                                    payload = messaging_event["message"][
-                                        "quick_reply"]["payload"]
-                                    if(len(payload) <= 5):
-                                        json_searchdogcat(sender_id, payload)
-                                    else:
-                                        typingon_json(sender_id)
-                                        crawler2(sender_id, payload)
-                                elif(message_text == u"全部種類" or message_text == u"狗" or message_text == u"貓"):
-                                    payload = messaging_event["message"][
-                                        "quick_reply"]["payload"]
-                                    if(payload == "dog " or payload == "cat "):
-                                        json_location2(sender_id, payload)
-                                    else:
-                                        json_searchbodytype(sender_id, payload)
-                                elif(message_text == u"全部體型" or message_text == u"迷你型" or message_text == u"小型" or message_text == u"中型" or message_text == u"大型"):
+                        
+                        if(get_flag(sender_id) == 1):
+                            chat(sender_id, message_text)
+                        elif(get_flag(sender_id) == 2):
+                            upload_db_intro(message_text, sender_id)
+                            json_message(sender_id, "已完成，請耐心等待神秘的明信片")
+                        elif(get_flag(sender_id) == 4 or
+                             get_flag(sender_id) == 5):
+                            '''
+                        use payload to save the data which user send
+                        領養資訊搜尋：location->city->kind->body->start crawler
+                        民間送養資訊：kind->location->city-> start crawler
+                            '''
+                            if(message_text == u"北部地區" or message_text == u"中部地區" or message_text == u"南部地區" or message_text == u"東部地區"):
+                                payload = messaging_event["message"][
+                                    "quick_reply"]["payload"]
+                                print "payload={a}".format(a=payload)
+                                if(payload == "1" or payload == "2" or payload == "3" or payload == "4"):
+                                    json_city(sender_id, payload)
+                                else:
+                                    json_city2(sender_id, payload)
+                            elif(u"縣" in message_text or u"市" in message_text or message_text == u"北北基宜全部" or message_text == u"桃竹苗全部" or message_text == u"中彰投全部" or message_text == u"雲嘉南全部" or message_text == u"高屏全部" or message_text == u"花東全部"):
+                                payload = messaging_event["message"][
+                                    "quick_reply"]["payload"]
+                                if(len(payload) <= 5):
+                                    json_searchdogcat(sender_id, payload)
+                                else:
                                     typingon_json(sender_id)
-                                    searchlist = messaging_event[
-                                        "message"]["quick_reply"]["payload"]
-                                    crawler(sender_id, searchlist)
+                                    crawler2(sender_id, payload)
+                            elif(message_text == u"全部種類" or message_text == u"狗" or message_text == u"貓"):
+                                payload = messaging_event["message"][
+                                    "quick_reply"]["payload"]
+                                if(payload == "dog " or payload == "cat "):
+                                    json_location2(sender_id, payload)
+                                else:
+                                    json_searchbodytype(sender_id, payload)
+                            elif(message_text == u"全部體型" or message_text == u"迷你型" or message_text == u"小型" or message_text == u"中型" or message_text == u"大型"):
+                                typingon_json(sender_id)
+                                searchlist = messaging_event[
+                                    "message"]["quick_reply"]["payload"]
+                                crawler(sender_id, searchlist)
                     elif("attachments" in messaging_event["message"] and get_flag(sender_id) == 2):
                         for attachment in messaging_event["message"]["attachments"]:
                             url = attachment["payload"]["url"]
